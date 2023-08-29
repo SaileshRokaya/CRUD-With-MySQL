79cb99579dacafeb4644f2262207895f206a4c58
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2023-08-29 06:13:29 +0545

    fix: create an api and react app

--- server/index.js
+++ server/index.js
@@ -1,25 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 // Establish a database connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'crudnode'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crudnode",
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res)=>{
-    res.send('Hello Express!');
+app.get("/api/get", (req, res) => {
+    const sqlGet = "SELECT * FROM contact_db";
+    connection.query(sqlGet, (error, result) => {
+      if (error) throw error;
+      res.send(result);
+    });
 })
 
-app.listen(5000, ()=>{
-    console.log('Server is running on port 5000');
-})
+app.get("/", (req, res) => {
+//   const sqlInsert =
+//     "INSERT INTO contact_db (name, email, contact) VALUES ('Sailesh','sailesh@gmail.com',9813065077)";
+//   connection.query(sqlInsert, (error, result) => {
+//     console.log("error", error);
+//     console.log("result", result);
+//     res.send("Hello Express!");
+//   });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
